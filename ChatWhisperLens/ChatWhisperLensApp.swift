23c4280e1a94df194ffc6a75a99c13afcfd2f246
Victor <victor@exigentia.com>
2025-05-23 11:18:33 +0200

    Initial Commit

---
+++ ChatWhisperLens/ChatWhisperLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWhisperLensApp.swift
+//  ChatWhisperLens
+//
+//  Created by (RE)SET on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatWhisperLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatWhisperLensTests/ChatWhisperLensTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatWhisperLensTests.swift
+//  ChatWhisperLensTests
+//
+//  Created by (RE)SET on 23/05/2025.
+//
+
+import Testing
+
+struct ChatWhisperLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
